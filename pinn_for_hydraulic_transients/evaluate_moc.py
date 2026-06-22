04b79fb8f61bc049c58fa885797ba2dd645988b7
Dmitry <shunko2000@gmail.com>
2026-06-22 22:51:19 +0700

    Результаты моделирования

--- pinn_for_hydraulic_transients/evaluate_moc.py
+++ pinn_for_hydraulic_transients/evaluate_moc.py
@@ -3,56 +3,56 @@ import matplotlib.pyplot as plt
 import scipy as sc
 from moc_train import ph_tensor1 as pf1
 from moc_train import ph_tensor2 as pf2
-from network import Network
+from Network_MLP import mMLP
 from openpyxl import Workbook
 import numpy as np
 from network_LAAF import DNN_LAAF
 
 
-layers = [2] + [20] * 8 + [2]
-model = DNN_LAAF(layers)
-model_loaded = torch.load('model.pth')
+layers = [2] + [24] * 4 + [2]
+model = mMLP(layers)
+model_loaded = torch.load('RU_PG.pth')
 model.load_state_dict(model_loaded)
 model.eval()
-testdata = sc.io.loadmat('Case_MOC_SFM/test.mat')
+testdata = sc.io.loadmat('RU_PG/train.mat')
 
-H_test = testdata['H_test']  # N x T
-T_test = testdata['t']  # T x 1
-X_test = testdata['X_test']  # N x 1
-V_test = testdata['V_test']
+X_test = testdata ['X']
+T_test = testdata ['t']
+Q_test = testdata ['F']
+alpha_test = testdata ['a']
 
 Xt = pf1(X_test)
 tt = pf1(T_test)
-Ht = pf2(H_test)
-Vt = pf2(V_test)
+Qt = pf2(Q_test)
+at = pf2(alpha_test)
 
-x00 = torch.linspace(10, 300, 30)
+x00 = torch.linspace(10, 500, 30)
 X = torch.stack(torch.meshgrid(Xt, tt)).reshape(2, -1).T
 x_test = X[:, 0]
 t_test = X[:, 1]
 
-h_test = Ht.flatten()[:, None]  # NT x 1
-v_test = Vt.flatten()[:, None]  # NT x 1
-hv_test = torch.cat([h_test, v_test], 1)
+Q_test = Qt.flatten()[:, None]  # NT x 1
+a_test = at.flatten()[:, None]  # NT x 1
+Qa_test = torch.cat([Q_test, a_test], 1)
 
 with torch.no_grad():
-    HV_pred = model(X).cpu()
+    Qa_pred = model(X).cpu()
 
 # Error
-error_h = torch.sqrt(torch.sum((HQ_pred[:, 0] - hq_test[:, 0]) ** 2) / torch.sum(hq_test[:, 0] ** 2))
+error_h = torch.sqrt(torch.sum((Qa_pred[:, 0] - Qa_test[:, 0]) ** 2) / torch.sum(Qa_test[:, 0] ** 2))
 print('Error : %e' % error_h)
 
-V0 = 0.412
-v_pred = HV_pred[:, 1]/100
+a0 = 38
+a_pred = Qa_pred[:, 1]
 
-V_pred = v_pred + (V0 - v_pred[0])  # PINN-Ye-2022
-error_v = torch.sqrt(torch.sum((hv_test[:, 1] - V_pred) ** 2) / torch.sum(hv_test[:, 1] ** 2))
-print('Error : %e' % error_v)
+a_pred = a_pred + (a0 - a_pred[0])  # PINN-Ye-2022
+error_a = torch.sqrt(torch.sum((Qa_test[:, 1] - a_pred) ** 2) / torch.sum(Qa_test[:, 1] ** 2))
+print('Error : %e' % error_a)
 
-HV_pred1 = torch.stack([HV_pred[:, 0], V_pred], 1)
-DATA = torch.cat([t_test.unsqueeze(1), HV_pred1, hv_test], 1)
+Qa_pred1 = torch.stack([Qa_pred[:, 0], a_pred], 1)
+DATA = torch.cat([t_test.unsqueeze(1), Qa_pred1, Qa_test], 1)
 DATA = DATA.numpy()
-HV_pred = HV_pred.numpy()
+Qa_pred = Qa_pred.numpy()
 
 list_data = DATA.tolist()
 workbook = Workbook()
