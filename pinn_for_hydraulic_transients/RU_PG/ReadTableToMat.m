eb8d84b0de1bed802063656577bf4747d7f1d816
Dmitry <shunko2000@gmail.com>
2026-06-22 10:07:52 +0700

    Новые данные

--- pinn_for_hydraulic_transients/RU_PG/ReadTableToMat.m
+++ pinn_for_hydraulic_transients/RU_PG/ReadTableToMat.m
@@ -1,10 +1,10 @@
 clc
 clear all
 Train=readmatrix('test.xlsx')
-X__test = [10];
+X_test = [10];
 F_test=Train(:,1)
 t_test=Train(:,2)
 a_test=Train(:,3)
-save test.mat a_test F_test t_test X__test 
+save test.mat a_test F_test t_test X_test 
 
 
